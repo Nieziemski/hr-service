744e45ddf28cfaef0147bf04909fa2a4a11ae9b2
Mikołaj Ziemski <mikolaj.nieziemski@gmail.com>
2019-04-28 20:37:15 +0200

    logo added, tabs removed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,35 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import React from "react";
+import { Link } from "gatsby";
+import logo from "../img/logo-simple.jpg";
 
 const Navbar = class extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       active: false,
-      navBarActiveClass: '',
-    }
+      navBarActiveClass: ""
+    };
   }
 
   toggleHamburger = () => {
     // toggle the active boolean in the state
     this.setState(
       {
-        active: !this.state.active,
+        active: !this.state.active
       },
       // after state has been updated,
       () => {
         // set the class in state for the navbar accordingly
         this.state.active
           ? this.setState({
-              navBarActiveClass: 'is-active',
+              navBarActiveClass: "is-active"
             })
           : this.setState({
-              navBarActiveClass: '',
-            })
+              navBarActiveClass: ""
+            });
       }
-    )
-  }
+    );
+  };
 
   render() {
     return (
@@ -41,8 +40,16 @@ const Navbar = class extends React.Component {
       >
         <div className="container">
           <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <Link to="/">
+              <img
+                src={logo}
+                alt="logo"
+                style={{
+                  height: "33px",
+                  "margin-top": "9px",
+                  "margin-right": "20px"
+                }}
+              />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -60,7 +67,7 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
+              {/*<Link className="navbar-item" to="/about">
                 About
               </Link>
               <Link className="navbar-item" to="/products">
@@ -68,15 +75,16 @@ const Navbar = class extends React.Component {
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
-              </Link>
+              </Link>*/}
               <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
+              {/*}<Link className="navbar-item" to="/contact/examples">
                 Form Examples
               </Link>
+              */}
             </div>
-            <div className="navbar-end has-text-centered">
+            {/*<div className="navbar-end has-text-centered">
               <a
                 className="navbar-item"
                 href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
@@ -87,12 +95,12 @@ const Navbar = class extends React.Component {
                   <img src={github} alt="Github" />
                 </span>
               </a>
-            </div>
+            </div>*/}
           </div>
         </div>
       </nav>
-    )
+    );
   }
-}
+};
 
-export default Navbar
+export default Navbar;
