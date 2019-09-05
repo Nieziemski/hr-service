17340444eb59850591cf3ab58b131f831d9062ac
Mikołaj Ziemski <mikolaj.nieziemski@gmail.com>
2019-09-05 18:49:07 +0200

    remove products

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -7,5 +7,4 @@ import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
